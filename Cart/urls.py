1f35515d19e42660419e33fe8a327fdf3167a830
Yi Sheng Lee <yisheng@live.com.sg>
2020-02-15 13:15:20 +0000

    Create add-to-cart function

--- Cart/urls.py
+++ Cart/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import add_to_cart
 
 urlpatterns = [
-  
+  path('add/<honey_id>',add_to_cart, name="add_cart"),
 ]
--- Cart/views.py
+++ Cart/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from Catalog.models import Product
 
-# Create your views here.
+def add_to_cart(request, honey_id):
+    cart = request.session.get('shopping_cart', {})
+    if honey_id not in cart:
+        product = get_object_or_404(Product, pk=honey_id)
+        # if product is found, add to the cart
+        cart[honey_id] = {
+            'id': honey_id,
+            'title': Product.name,
+            'price': Product.price
+        }
+        # save the cart back to sessions
+        request.session['shopping_cart'] = cart
+              
+        messages.success(request, "Your product has been added to your cart!")
+        return redirect('show_products')
+        
+    else:
+        return redirect('show_products')
