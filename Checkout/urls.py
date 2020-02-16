f306d83a6ba03bc2a08bab0bb4f1517f4b36d373
Yi Sheng Lee <yisheng@live.com.sg>
2020-02-16 14:39:20 +0000

    Added checkout success

--- Checkout/urls.py
+++ Checkout/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import donate
+from .views import donate, checkout_success
 
 urlpatterns = [
     
     path('donate/', donate, name='donate'),
+    path('success/', checkout_success),
 ]
--- Checkout/views.py
+++ Checkout/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, reverse, HttpResponse, get_object_or_404
+from .forms import OrderForm, PaymentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils import timezone
 
 #import settings so that we can access the public stripe key
 from django.conf import settings
@@ -6,5 +10,13 @@ import stripe
 
 from Catalog.models import Product
 
+# Donate Section
 def donate(request):
     return render(request, 'donate.template.html')
+
+
+            
+
+# Checkout Section
+def checkout_success(request):
+    return render(request, 'checkout_success.template.html')
